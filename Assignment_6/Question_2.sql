73eb35517fd1a4d18d98816ded15fce23dbf9c71
kavinraj23799@gmail.com <Kavin@172>
2024-04-09 14:08:18 +0530

    fix: Assignment 6

---
+++ Assignment_6/Question_2.sql
@@ -0,0 +1,7 @@
+INSERT INTO employee (empcode, empname, empage, esalary) 
+VALUES 
+    (101, 'Jenny', 25, 10000),
+    (102, 'Jacky', 30, 20000),
+    (103, 'Joe', 20, 40000),
+	 (101, 'John', 40, 80000),
+    (102, 'Shameer', 25, 90000);
